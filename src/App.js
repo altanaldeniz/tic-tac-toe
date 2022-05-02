e6bea1da549d97e5c0d4c35ea0dd34d05757ecf0
Altan Aldeniz <altanaldeniz@gmail.com>
2022-05-02 06:40:04 +0300

    remove borders

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Board from "./components/Board";
 
 function App() {
   return (
-    <div className="flex flex-col items-center border-4">
-      <div className="border-4 p-3 m-5">
+    <div className="flex flex-col items-center">
+      <div className="p-3 m-5">
         <h1 className="text-4xl my-4">Tic Tac Toe</h1>
         <hr className=" min-w-max" />
       </div>
       <Board />
-      <footer className="border-4 p-3 m-5">
+      <footer className="p-3 m-5">
         <span>
           View the source code on{" "}
           <a href="https://github.com/altanaldeniz">GitHub</a>
