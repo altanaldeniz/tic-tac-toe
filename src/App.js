58e081fddc9a10f9bf33a6475d4d1e2755b2ef7a
Altan Aldeniz <altanaldeniz@gmail.com>
2022-05-02 05:30:32 +0300

    title and board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Board from "./components/Board";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center border-4">
+      <div className="border-4 p-3 m-5">
+        <h1 className="text-4xl my-4">Tic Tac Toe</h1>
+        <hr className=" min-w-max" />
+      </div>
+      <Board />
     </div>
   );
 }
