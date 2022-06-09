a469e0690f72ea7d99d05c22648071f14b0c05f8
Altan Aldeniz <altanaldeniz@gmail.com>
2022-06-09 06:43:39 -0700

    adjusted height

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import Board from "./components/Board";
+import Board from './components/Board';
 
 function App() {
   return (
     <div className="flex flex-col items-center">
-      <div className="p-3 m-5">
-        <h1 className="text-4xl my-4">Tic Tac Toe</h1>
+      <div className="">
+        <h1 className="text-4xl my-2">Tic Tac Toe</h1>
         <hr className="min-w-max" />
       </div>
       <Board />
-      <footer className="p-3 m-5">
+      <footer className="p-3">
         <span>
-          View the source code on{" "}
+          View the source code on{' '}
           <a
             className="hover:underline text-blue-500"
             href="https://github.com/altanaldeniz/tic-tac-toe"
